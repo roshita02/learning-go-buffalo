c4f617070749beeface59391bcdb90d431bf6a5a
roshita-shakya <roshitashakya@lftechnology.com>
2022-01-05 00:43:06 +0545

    Add validation tests on user model

--- models/user_test.go
+++ models/user_test.go
@@ -6,4 +6,13 @@ func (ms *ModelSuite) Test_User() {
 		LastName: "Doe",
 	}
 	ms.Equal("John Doe", u.FullName(), "FullName returns user name.")
+
+	db := ms.DB
+	verrs, err := db.ValidateAndCreate(u)
+	if err != nil {
+		panic(err)
+	}
+
+	ms.NotNil(u.ID, "User ID is generated when saved to DB.")
+	ms.True(verrs.HasAny(), "User cannot be created without age field.")
 }
